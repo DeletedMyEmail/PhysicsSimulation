7cb0aec259cb378f81c40910d6ebd356938e60b4
DeletedMyEmail <kaitokuntatsu1@gmail.com>
2024-05-15 11:00:30 +0200

    fix reading shader

--- src/shader/Shader.cpp
+++ src/shader/Shader.cpp
@@ -2,31 +2,50 @@
 #include <fstream>
 #include <sstream>
 #include <exception>
+#include <iostream>
 
 class ShaderNotFoundException final : std::exception {};
 
-GLuint createShader(const char* pVertexShaderSource, const char* pFragmentShaderSource) {
-    std::ifstream lVertexFile(pVertexShaderSource);
-    std::ifstream lFragFile(pFragmentShaderSource);
+std::string getShaderSource(const char* pShaderSource) {
+    std::ifstream lShaderFile(pShaderSource);
 
-    if (!lVertexFile.is_open() || !lFragFile.is_open())
+    if (!lShaderFile.is_open())
         throw ShaderNotFoundException();
 
-    std::stringstream lVertexBuffer, lFragBuffer;;
-    lFragBuffer << lFragFile.rdbuf();
-    lVertexBuffer << lVertexFile.rdbuf();
+    std::stringstream lShaderBuffer;
+    lShaderBuffer << lShaderFile.rdbuf();
 
-    const char* lVertexString = lVertexBuffer.str().c_str();
-    const char* lFragString = lFragBuffer.str().c_str();
+    return lShaderBuffer.str();
+}
 
-    GLuint lVertexShader = glCreateShader(GL_VERTEX_SHADER);
-    GLuint lFragShader = glCreateShader(GL_FRAGMENT_SHADER);
+GLuint createShader(const char* pVertexShaderSource, const char* pFragmentShaderSource) {
+    const std::string lVertexString = getShaderSource(pVertexShaderSource);
+    const std::string lFragString = getShaderSource(pFragmentShaderSource);
 
-    glShaderSource(lVertexShader, 1, &lVertexString, nullptr);
-    glShaderSource(lFragShader, 1, &lFragString, nullptr);
+    const char* lVertexCString = lVertexString.c_str();
+    const char* lFragCString = lFragString.c_str();
+
+    const GLuint lVertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const GLuint lFragShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(lVertexShader, 1, &lVertexCString, nullptr);
+    glShaderSource(lFragShader, 1, &lFragCString, nullptr);
 
     glCompileShader(lVertexShader);
     glCompileShader(lFragShader);
+
+    GLint success;
+    // Überprüfen Sie die Kompilierung des Vertex-Shaders
+    glGetShaderiv(lVertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << std::endl;
+    }
+
+    // Überprüfen Sie die Kompilierung des Fragment-Shaders
+    glGetShaderiv(lFragShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << std::endl;
+    }
 
     GLuint lShaderProgram = glCreateProgram();
     glAttachShader(lShaderProgram, lVertexShader);
